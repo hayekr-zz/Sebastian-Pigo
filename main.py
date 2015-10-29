407904d772feb6d7958e36f4c8fbe7979b65f9b9
Sebastian Williams <sebomonz@gmail.com>
2015-10-29 09:00:50 -0400

    Added some other functions

--- main.py
+++ main.py
@@ -1,9 +1,29 @@
 __author__ = 'Sebastian'
-#from gopigo import *
+from gopigo import *
+import time
 
 class Pigo:
+
+    isMoving = False
 
     def __init__(self):
         print "Boop Beep"
 
+    #named after method stop() in GoPiGo lib
+    def stop(self):
+        self.isMoving = False
+        while stop() != 1:
+            time.sleep(0.1)
+            print "ERROR 01: Can't stop"
+
+    #named after method fwd() in GoPiGo lib
+    def fwd(self):
+        self.isMoving = True
+        while fwd() != 1:
+            time.sleep(0.1)
+            print "ERROR 02: Can't move fwd"
+
 myPigo = Pigo()
+myPigo.fwd()
+time.sleep(2)
+myPigo.stop()
